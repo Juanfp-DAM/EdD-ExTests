0191dd5d5fa0eeb4960f693ce55f578a758bc910
dam_fpj <juaferpui@alu.edu.gva.es>
2024-04-16 13:04:14 +0200

    Commit con los 4 primeros test, arreglar redondeo

--- src/es/iesmz/tests/Money.java
+++ src/es/iesmz/tests/Money.java
@@ -2,12 +2,11 @@ package es.iesmz.tests;
 
 public class Money {
 
-    /*private float cantidad;
-    private enum <TipoMoneda> tipo;*/
 
 
-    float change(TipoMoneda origen, TipoMoneda destino, float money){
-        float monedaConvertida=0;
+    public static float change(TipoMoneda origen, TipoMoneda destino, float money){
+        float monedaConvertida=0f;
+        float monedaIntercambio;
         if(money>=0) {
             if (origen.equals(TipoMoneda.EUR)) {
                 if (destino.equals(TipoMoneda.USD)) {
@@ -19,20 +18,26 @@ public class Money {
                 if (destino.equals(TipoMoneda.EUR)) {
                     monedaConvertida = (float) (money * 1.165826);
                     //TODO hacer cambio GBP DOLAR
-                }/*else if(destino.equals(TipoMoneda.USD)){
-                monedaConvertida=(float)(money*)
-            }*/
+                }else if(destino.equals(TipoMoneda.USD)){
+                monedaIntercambio=(float)(money*1.165826);
+                monedaConvertida=(float)(monedaIntercambio*1.18798);
+            }
             } else if (origen.equals(TipoMoneda.USD)) {
                 if (destino.equals(TipoMoneda.EUR)) {
                     monedaConvertida = (float) (money * 0.841815);
+                }else if(destino.equals(TipoMoneda.GBP)){
+                    monedaIntercambio=(float)(money*0.841815);
+                    monedaConvertida=(float)(monedaIntercambio*0.857839);
                 }
                 //TODO hacer cambio USD a GBP
+
             } else {
                 monedaConvertida = -1;
             }
         }else{
             monedaConvertida=-1;
         }
+
         return monedaConvertida;
     }
 }
--- src/es/iesmz/tests/MoneyTest.java
+++ src/es/iesmz/tests/MoneyTest.java
@@ -1,5 +1,7 @@
 package es.iesmz.tests;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
@@ -7,11 +9,52 @@ class MoneyTest {
     @org.junit.jupiter.api.Test
     void tipoCambio1(){
         //ARRANGE
-
+        TipoMoneda origen = TipoMoneda.EUR;
+        TipoMoneda destino= TipoMoneda.USD;
+        float money=23.88f;
         //ACT
-
+        float resultado=Money.change(origen,destino,money);
         //ASSERT
+        //assertEquals(28.37f,resultado);
+        assertEquals(28.37f,resultado);
     }
 
+
+    @Test
+    void tipoCambio2(){
+        //ARRANGE
+        TipoMoneda origen=TipoMoneda.GBP;
+        TipoMoneda destino=TipoMoneda.EUR;
+        float money=1000.0f;
+        //ACT
+        float resultado=Money.change(origen, destino,money);
+        //ASSERT
+        assertEquals(1165.83f,resultado);
+    }
+
+
+    @Test
+    void tipoCambio3(){
+        //ARRANGE
+        TipoMoneda origen=TipoMoneda.EUR;
+        TipoMoneda destino=TipoMoneda.GBP;
+        float money=234.56f;
+        //ACT
+        float resultado=Money.change(origen, destino,money);
+        //ASSERT
+        assertEquals(201.21f,resultado);
+    }
+
+    @Test
+    void tipoCambio4(){
+        //ARRANGE
+        TipoMoneda origen=TipoMoneda.USD;
+        TipoMoneda destino=TipoMoneda.EUR;
+        float money=44.56f;
+        //ACT
+        float resultado=Money.change(origen, destino,money);
+        //ASSERT
+        assertEquals(37.51f,resultado);
+    }
 
 }
